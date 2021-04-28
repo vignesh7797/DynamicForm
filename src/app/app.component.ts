b8b54dd6b5332bce6098465ccd593ea69e1c4379
vignesh <vickyrobotic7@outlook.com>
2021-04-28 17:31:20 +0530

    UI developed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'DynamicForm';
+
+  dynamicFormArray: any;
+  states: any;
+  district:any;
+  registerForm:FormGroup;
+
+  constructor(private http: HttpClient, private builder:FormBuilder) {   }
+
+  ngOnInit(){
+
+    this.registerForm = this.builder.group({
+
+    });
+
+
+    this.http.get("/assets/form.json").subscribe(data => {
+      this.dynamicFormArray = data;
+      console.log(this.dynamicFormArray);
+    });
+    this.http.get("/assets/states.json").subscribe(data => {
+      this.states = data;
+      console.log(this.states);
+      this.district=this.states[0].districts;
+      this.formControl();
+    });
+    
+  }
+
+
+  formControl(){
+    this.dynamicFormArray.forEach(element =>{
+      this.registerForm.addControl(element.id, new FormControl(''));
+    });
+
+    console.log(this.registerForm.controls)
+  }
+
+
+
+  OnSelectState(e){
+    let index =this.states.map((s)=> s.state).indexOf(e);
+    this.district = this.states[index].districts;
+    console.log(e, index)
+  }
+
 }
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
+
+
+
+
+
+
+
+@NgModule({
+  declarations: [],
+  exports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    MatNativeDateModule 
+   ]
+})
+export class MaterialModule { }
