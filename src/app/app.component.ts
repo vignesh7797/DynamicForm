82a8dd9c1d6cf01e88f2665e58e16edd1235b3ad
vignesh <vickyrobotic7@outlook.com>
2021-04-30 15:47:27 +0530

    xx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
+import { DatePipe } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Inject, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+export interface Data{
+  FormData;
+}
 
 @Component({
   selector: 'app-root',
@@ -15,22 +21,27 @@ export class AppComponent implements OnInit{
   district:any;
   registerForm:FormGroup;
   submittedData;
+  maxdate = new Date().toISOString().split("T")[0];
 
 
   constructor(private http: HttpClient, private builder:FormBuilder) {  
 
-    this.registerForm = this.builder.group({
-      FirstName:[null, Validators.required],
-      LastName:[null, Validators.required],
-      EmailId:[null, [Validators.required, Validators.email]],
-      PhoneNo:[null, [Validators.required, Validators.max(9999999999), Validators.min(1000000000)]],
-      Gender:[null],
-      DOB:[null, [Validators.required]],
-      Address:[null, Validators.required],
-      State:[null, Validators.required],
-      District:[null, Validators.required],
-      Pincode:[null, [Validators.required, Validators.max(999999), Validators.min(100000)]]
-    });
+    // this.registerForm = this.builder.group({
+    //   FirstName:[null, Validators.required],
+    //   LastName:[null, Validators.required],
+    //   EmailId:[null, [Validators.required, Validators.email]],
+    //   PhoneNo:[null, [Validators.required, Validators.max(9999999999), Validators.min(1000000000)]],
+    //   Gender:[null],
+    //   DOB:[null, [Validators.required]],
+    //   Address:[null, Validators.required],
+    //   State:[null, Validators.required],
+    //   District:[null, Validators.required],
+    //   Pincode:[null, [Validators.required, Validators.max(999999), Validators.min(100000)]]
+    // });
+
+    this.registerForm = this.builder.group({});
+
+    
 
    }
 
@@ -40,37 +51,70 @@ export class AppComponent implements OnInit{
 
   ngOnInit(){
 
-    this.http.get("/assets/form.json").subscribe(data => {
-      this.dynamicFormArray = data;
+    this.http.get("/assets/form.json").subscribe((res: Response) => {
+      this.dynamicFormArray = res;
       console.log(this.dynamicFormArray);
+      this.newFormControl();
     });
-    
+
     this.http.get("/assets/states.json").subscribe(data => {
       this.states = data;
       console.log(this.states);
       this.district=[];
-      
     });    
     
   }
 
+
+  newFormControl(){
+
+    this.dynamicFormArray.forEach(item =>{
+      if(item.Validation.required == true){
+          if(item.Validation.type == 'text' || !item.Validation.type){
+            this.registerForm.addControl(item.id, new FormControl(null, Validators.required));
+          }else if(item.Validation.type == 'email'){
+            this.registerForm.addControl(item.id, new FormControl(null, [Validators.required, Validators.email]));
+          }else if(item.Validation.type == 'number'){
+            this.registerForm.addControl(item.id, new FormControl(null, [Validators.required, Validators.min(item.Validation.min), Validators.max(item.Validation.max)]))
+          }
+      }else{
+        this.registerForm.addControl(item.id, new FormControl(null));
+      }
+    });
+
+    console.log(this.registerForm);
+  }
+
+
   
-
-
-
   OnSelectState(e){
     let index =this.states.map((s)=> s.state).indexOf(e);
     this.district = this.states[index].districts;
     console.log(e, index)
   }
 
-  OnSubmit(){
-   if(this.registerForm.valid){
-    this.submittedData =JSON.parse(this.registerForm.value);
-    console.log(this.submittedData);
-   }
+
+
+  CountDigits(num){
+    return num.toString().length;
   }
 
+
+
+  OnSubmit(){
+    if(this.registerForm.valid){
+     this.submittedData =this.registerForm.getRawValue();
+     console.log(this.registerForm.value);
+     this.submittedData.DOB = new DatePipe('en-US').transform(this.submittedData.DOB, 'MMMM d, y');
+
+     this.registerForm.reset();
+
+    Object.keys(this.registerForm.controls).forEach(key => {
+      this.registerForm.get(key).setErrors(null) ;
+    });
+    }
+   }
   
 
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CommonModule ,
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
